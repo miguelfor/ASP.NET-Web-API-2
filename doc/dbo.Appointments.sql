e78ab9f3c4edbc959820f8dc15c67075b0615004
“MiguelFor” <miguelfor@hotmail.com>
2017-04-28 20:26:48 -0500

    ApplicationInsights y SQL tabla Appointment

---
+++ doc/dbo.Appointments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Appointments] (
+    [ID]        INT      IDENTITY (1, 1) NOT NULL,
+    [IdDoctor]  INT      NOT NULL,
+    [IdPatient] INT      NOT NULL,
+    [Date]      DATETIME NOT NULL,
+    [Status]    INT      NOT NULL,
+    CONSTRAINT [PK_dbo.Appointments] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
